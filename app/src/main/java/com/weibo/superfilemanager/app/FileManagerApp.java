5aab9dddc6f2651530a4be525467c3754e5953cf
weibo <gt1254094162@163.com>
2017-05-01 15:49:37 +0800

    20170501添加logger框架

--- app/src/main/java/com/weibo/superfilemanager/app/FileManagerApp.java
+++ app/src/main/java/com/weibo/superfilemanager/app/FileManagerApp.java
@@ -2,6 +2,8 @@ package com.weibo.superfilemanager.app;
 
 import android.app.Application;
 
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by weibo on 17-5-1.
  */
@@ -10,5 +12,6 @@ public class FileManagerApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Logger.init("sout");
     }
 }
